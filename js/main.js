b4899ae26b1565341f8ffaab1d2815e834939f35
martaguillemolmos <martetaguillem@gmail.com>
2023-09-25 18:37:47 +0200

    Realizamos la primera función de encender y apagar el piloto de la luz y la pantalla al hacer click sobre el id=encender.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 // FUNCIÓN ENCENDER Y APAGAR EL PILOTO DE LA LUZ al pulsar sobre ON-OFF.
-console.log ("1");
-const encender= document.getElementById ('encender');
-const piloto =document.getElementById ('piloto');
-const pantalla =document.getElementById('pantalla');
-console.log ("2");
+console.log("1");
+
+const encender = document.getElementById("encender");
+let piloto = document.getElementById("piloto");
+let pantalla = document.getElementById("pantalla");
 
 encender.addEventListener('click', function () {
-    console.log ("3")
-if (piloto.style.backgroundColor === "green"){
+    console.log ("3");
+if (piloto.style.backgroundColor === "green" || piloto.style.backgroundColor === ''){
     console.log ("4");
     piloto.style.backgroundColor = "red";
     pantalla.style.backgroundColor = "blue";
- }else {
+ } else {
         piloto.style.backgroundColor = "green";
         pantalla.style.backgroundColor = "green";
         console.log("5")
-    } 
+}
 
 });
-
-
