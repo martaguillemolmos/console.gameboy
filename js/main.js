eec7fdd9713097cfc9b24610f2742ff01e7dccc1
martaguillemolmos <martetaguillem@gmail.com>
2023-09-25 20:41:26 +0200

    Realizamos las siguientes modificaciones en los documentos main.js, así como HTML y style: - main.js: Hemos incluido la función de los botones A y B así como los botones de select y reset. - html y css: Hemos añadido una canción al encender la Nintendo.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,53 @@
 const encender = document.getElementById("encender");
 let piloto = document.getElementById("piloto");
 let pantalla = document.getElementById("pantalla");
+var audio = document.getElementById("audio");
 
 encender.addEventListener("click", function () {
   if (
     piloto.style.backgroundColor === "green" ||
     piloto.style.backgroundColor === ""
   ) {
+    audio.play();
     piloto.style.backgroundColor = "red";
-    pantalla.style.backgroundImage = "url('img/logonintendo.png')";
+    pantalla.style.backgroundImage = "url('img/inicio.png')";
   } else {
     piloto.style.backgroundColor = "green";
     pantalla.style.backgroundImage = "";
     pantalla.style.backgroundColor = "green";
+  }
+});
+
+// FUNCIÓN CAMBIAR DE PANTALLA, BOTON A Y B
+
+let botonA = document.getElementById("botonA");
+let botonB = document.getElementById("botonB");
+
+botonA.addEventListener("click", function () {
+  if (piloto.style.backgroundColor === "red") {
+    pantalla.style.backgroundImage = "url('img/logonintendo.png')";
+  }
+});
+
+botonB.addEventListener("click", function () {
+  if (piloto.style.backgroundColor === "red") {
+    pantalla.style.backgroundImage = "url('img/inicio.png')";
+  }
+});
+
+// BOTON SELECT Y RESET
+
+let select = document.getElementById("selectBoton");
+let reset = document.getElementById("clickStart");
+
+select.addEventListener("click", function () {
+  if (piloto.style.backgroundColor === "red") {
+    pantalla.style.backgroundImage = "url('img/logonintendo.png')";
+  }
+});
+
+reset.addEventListener("click", function () {
+  if (piloto.style.backgroundColor === "red") {
+    pantalla.style.backgroundImage = "url('img/inicio.png')";
   }
 });
